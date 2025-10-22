6c2b35856f604dad9a4937f66c9fd830a6f166d7
S. Kozyr <s.trump@gmail.com>
2025-10-22 22:18:57 +0300

    Small fix for \n unescape in Android strings.xml

--- python_twine/twine/formatters/android.py
+++ python_twine/twine/formatters/android.py
@@ -152,6 +152,7 @@ class AndroidFormatter(AbstractFormatter):
                 # Add tail text if any (text after the last child element)
                 # Note: child.tail is text AFTER the element, not inside
 
+                value = value.replace("\n\\n", "\n")
                 self.set_translation_for_key(key, lang, value, current_section)
 
                 if comment:
