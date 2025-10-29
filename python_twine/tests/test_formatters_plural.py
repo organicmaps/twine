61cc105ff5b95b88b2cebc66159d12dd6fc88c03
S. Kozyr <s.trump@gmail.com>
2025-10-29 15:52:24 +0200

    Added tests for Android and iPhone plurals formats

--- python_twine/tests/test_formatters_plural.py
+++ python_twine/tests/test_formatters_plural.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import pytest
 
+from twine.formatters.apple_plural import ApplePluralFormatter
 from twine.twine_file import TwineFile, TwineDefinition, TwineSection
 from twine.formatters.android import AndroidFormatter
 
@@ -26,6 +27,33 @@ class TestAndroidPluralFormatter:
         """Get fixtures directory path."""
         return Path(__file__).parent / "fixtures"
 
+    @pytest.fixture
+    def twine_file(self):
+        # Prepare TwineFile data
+        twine_file = TwineFile()
+        num_edits_def = TwineDefinition("num_edits")
+
+        twine_file.definitions_by_key["num_edits"] = num_edits_def
+        twine_file.language_codes = ["en", "de"]
+        twine_file.sections = [TwineSection("OSM")]
+        twine_file.sections[0].definitions.append(num_edits_def)
+
+        # Put plural translation
+        num_edits_def.plural_translations["en"] = {
+            "one": "%d edit",
+            "other": "%d edits"
+        }
+        num_edits_def.translations["en"] = "%d edits"
+
+        num_edits_def.plural_translations["de"] = {
+            "zero": "%d Bearbeitungen",
+            "one": "%d Bearbeitung",
+            "other": "%d Bearbeitungen"
+        }
+        num_edits_def.translations["de"] = "%d Bearbeitungen"
+
+        return twine_file
+
     def test_read_format(self, formatter, fixtures_dir):
         """Test reading Android XML format with <plural/> tags."""
         fixture_path = fixtures_dir / "formatter_android_plurals.xml"
@@ -41,6 +69,34 @@ class TestAndroidPluralFormatter:
         assert "tracks" in twine_file.definitions_by_key
         translations2 = twine_file.definitions_by_key["tracks"].plural_translations
         assert translations2 == {"en": {"one": "%d track", "other": "%d tracks"}}
+
+    def test_write_format(self, formatter, twine_file):
+        formatter.twine_file = twine_file
+
+        en_plural_content = formatter.format_file("en")
+        assert en_plural_content == """<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <!-- SECTION: OSM -->
+    <plurals name="num_edits">
+        <item quantity="one">%d edit</item>
+        <item quantity="other">%d edits</item>
+    </plurals>
+</resources>
+"""
+
+        de_plural_content = formatter.format_file("de")
+        assert de_plural_content == """<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <!-- SECTION: OSM -->
+    <plurals name="num_edits">
+        <item quantity="zero">%d Bearbeitungen</item>
+        <item quantity="one">%d Bearbeitung</item>
+        <item quantity="other">%d Bearbeitungen</item>
+    </plurals>
+</resources>
+"""
 
 
 class TestTwineFilePlural:
@@ -105,3 +161,100 @@ class TestTwineFilePlural:
             "many": "%d меток",
             "other": "%d меток"
         }
+
+
+class TestApplePluralFormatter:
+    @pytest.fixture
+    def formatter(self):
+        """Create formatter with empty TwineFile."""
+        formatter = ApplePluralFormatter()
+        formatter.options = {"consume_all": True, "consume_comments": True}
+        return formatter
+
+    @pytest.fixture
+    def twine_file(self):
+        # Prepare TwineFile data
+        twine_file = TwineFile()
+        num_edits_def = TwineDefinition("num_edits")
+
+        twine_file.definitions_by_key["num_edits"] = num_edits_def
+        twine_file.language_codes = ["en", "de"]
+        twine_file.sections = [TwineSection("OSM")]
+        twine_file.sections[0].definitions.append(num_edits_def)
+
+        # Put plural translation
+        num_edits_def.plural_translations["en"] = {
+            "one": "%d edit",
+            "other": "%d edits"
+        }
+        num_edits_def.translations["en"] = "%d edits"
+
+        num_edits_def.plural_translations["de"] = {
+            "zero": "%d Bearbeitungen",
+            "one": "%d Bearbeitung",
+            "other": "%d Bearbeitungen"
+        }
+        num_edits_def.translations["de"] = "%d Bearbeitungen"
+
+        return twine_file
+
+    def test_write_plural_format(self, formatter, twine_file):
+        formatter.twine_file = twine_file
+
+        en_plural_content = formatter.format_file("en")
+        assert en_plural_content == """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+
+<!-- ********** OSM **********/ -->
+
+\t<key>num_edits</key>
+\t<dict>
+\t\t<key>NSStringLocalizedFormatKey</key>
+\t\t<string>%#@value@</string>
+\t\t<key>value</key>
+\t\t<dict>
+\t\t\t<key>NSStringFormatSpecTypeKey</key>
+\t\t\t<string>NSStringPluralRuleType</string>
+\t\t\t<key>NSStringFormatValueTypeKey</key>
+\t\t\t<string>d</string>
+\t\t\t<key>one</key>
+\t\t\t<string>%d edit</string>
+\t\t\t<key>other</key>
+\t\t\t<string>%d edits</string>
+\t\t</dict>
+\t</dict>
+
+</dict>
+</plist>"""
+
+        de_plural_content = formatter.format_file("de")
+        assert de_plural_content == """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+
+<!-- ********** OSM **********/ -->
+
+\t<key>num_edits</key>
+\t<dict>
+\t\t<key>NSStringLocalizedFormatKey</key>
+\t\t<string>%#@value@</string>
+\t\t<key>value</key>
+\t\t<dict>
+\t\t\t<key>NSStringFormatSpecTypeKey</key>
+\t\t\t<string>NSStringPluralRuleType</string>
+\t\t\t<key>NSStringFormatValueTypeKey</key>
+\t\t\t<string>d</string>
+\t\t\t<key>zero</key>
+\t\t\t<string>%d Bearbeitungen</string>
+\t\t\t<key>one</key>
+\t\t\t<string>%d Bearbeitung</string>
+\t\t\t<key>other</key>
+\t\t\t<string>%d Bearbeitungen</string>
+\t\t</dict>
+\t</dict>
+
+</dict>
+</plist>"""
